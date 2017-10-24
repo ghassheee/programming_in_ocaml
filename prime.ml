85757b633924a813788bef8036582e0f14f037e1
ghassheee <ghassheee@gmail.com>
2017-10-24 20:17:31 +0900

    Updated

--- prime.ml
+++ prime.ml
@@ -1,3 +1,11 @@
+(* initialize prime db 
+ *
+ * $ mkdir prime
+ * $ ocaml
+ * ocaml # #use "base.ml";;
+ * ocaml # mk_prime_db 1 10;;
+ *
+ *)
 
 #directory "prime"
 #use "prime_tbl_1_100000.ml"
@@ -26,25 +34,23 @@ let p = concat [
     prime_tbl_900001_1000000]
 
 
+let     print_bald_list l   = foldr(fun x->(^)(soi x^";")) "" l 
+let     print_list l        = "[" ^ print_bald_list l ^ "]" 
+let rec prime_tbl m n       = if m>n then [] else 
+    if is_prime m then m::prime_tbl(m+1)n else prime_tbl (m+1) n ;;
 
-let rec prime_tbl m n   =  
-        if m>n then [] else 
-        if is_prime m then m::prime_tbl(m+1)n else prime_tbl (m+1) n;;
-
-let print_intlist l = "["^(foldr(fun x->(^)(soi x^"; "))"]" l) ;;
-let print_primes l = foldr(fun x->(^)(soi x^";")) "" l ;;
-
-let mk_prime_tbl m n = 
-    let tbl         = "prime_tbl_"^soi m^"_"^soi n in 
-    let fn          = "prime/" ^ tbl ^ ".ml" in
-    let fd          = open_out fn in 
-    let print_start = output_bytes fd ("let "^tbl^" = [") in
-    let loop m n fd = output_bytes fd (print_primes (prime_tbl m n)) in
-    let size        = (n-m-1)/10 in
-    let m'          = (m-1)/size in let n' = n/size in 
-    let iter        = rev (m' -- n') in
-    let ()= foldr (fun x xs->let()=loop(x*size+1)((x+1)*size)fd in xs)() iter in 
-    let print_end   = output_bytes fd "];;" in
+let     mk_prime_tbl m n    = 
+    let tbl                 = "prime_tbl_" ^ soi m ^ "_" ^ soi n                in 
+    let fn                  = "prime/" ^ tbl ^ ".ml"                            in
+    let fd                  = open_out fn                                       in 
+    let loop m n fd         = output_bytes fd(print_bald_list(prime_tbl m n))   in
+    let size                = (n-m-1)   /   10                                  in
+    let m'                  = (m-1)     /   size                                in 
+    let n'                  = n         /   size                                in 
+    let iter                = rev (m' -- n') in
+    let () = output_bytes fd ("let " ^ tbl ^ " = [") in
+    let () = foldr(fun x xs->let()=loop(x*size+1)((x+1)*size)fd in xs)()iter in 
+    let () = output_bytes fd "];;" in
     close_out fd;;  
 
 let pdb_size = 100000
